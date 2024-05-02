b3d385433249a1e43b57acf8f0a848b9c5184274
heather-jp <163785729+heather-jp@users.noreply.github.com>
2024-05-02 23:22:36 +0100

    Updates to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "a304144904abba9o32da5fa19eftfe9a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+  // make api call and update interface
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
